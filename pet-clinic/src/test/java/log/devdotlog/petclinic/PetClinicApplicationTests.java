39d9aac702df8487300d5f59143b1f41bc753e9b
leole <leolenkhatho@gmail.com>
2023-10-31 14:00:56 +0200

    Initial Project Add

---
+++ pet-clinic/src/test/java/log/devdotlog/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package log.devdotlog.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
