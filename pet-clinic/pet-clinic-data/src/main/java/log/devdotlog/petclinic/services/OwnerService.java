e92e23450aa6cec3ef5b0a278f388910f35681fa
leole <leolenkhatho@gmail.com>
2023-11-07 07:16:30 +0200

    Implemented interfaces for services. Closes #5.

---
+++ pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package log.devdotlog.petclinic.services;
+
+import log.devdotlog.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package log.devdotlog.petclinic.services;
+
+import log.devdotlog.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
