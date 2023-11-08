a99055ba47c8ce4ccf0e0adde5f8b23646a262a5
leole <leolenkhatho@gmail.com>
2023-11-08 14:45:06 +0200

    Added index and controller. Closes #8.

---
+++ pet-clinic/pet-clinic-web/src/main/java/log/devdotlog/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package log.devdotlog.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
