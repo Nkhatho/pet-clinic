5cf3304e4814a167d6d9c3072ce53784060be810
leole <leolenkhatho@gmail.com>
2023-11-12 14:26:14 +0200

    Added Vets List. Closes #15

--- pet-clinic/pet-clinic-web/src/main/java/log/devdotlog/petclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/log/devdotlog/petclinic/controllers/VetController.java
@@ -1,12 +1,19 @@
 package log.devdotlog.petclinic.controllers;
 
+import log.devdotlog.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
     @RequestMapping({ "/vets"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
