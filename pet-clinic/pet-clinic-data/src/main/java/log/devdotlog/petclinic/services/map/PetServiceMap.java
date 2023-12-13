85ea44851ec914c70e46ebd350ef9574891f5c57
leole <leolenkhatho@gmail.com>
2023-12-13 16:13:13 +0200

    Added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/services/map/PetServiceMap.java
+++ pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package log.devdotlog.petclinic.services.map;
 
 import log.devdotlog.petclinic.model.Pet;
 import log.devdotlog.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic/pet-clinic-web/src/main/java/log/devdotlog/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/log/devdotlog/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package log.devdotlog.petclinic.bootstrap;
 
 import log.devdotlog.petclinic.model.Owner;
+import log.devdotlog.petclinic.model.Pet;
 import log.devdotlog.petclinic.model.PetType;
 import log.devdotlog.petclinic.model.Vet;
 import log.devdotlog.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import log.devdotlog.petclinic.services.PetTypeService;
 import log.devdotlog.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,12 +36,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Big Street");
+        owner1.setCity("Joburg");
+        owner1.setTelephone("947 144 4234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glen");
+        owner2.setAddress("321 Small Street");
+        owner2.setCity("Joburg");
+        owner2.setTelephone("241 424 1313");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("JusCat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
         System.out.println("Loaded Owners ...");
