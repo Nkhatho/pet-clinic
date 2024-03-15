c36d3e2dc7fad89ac60ec5d2dafa6279824a39d9
leole <leolenkhatho@gmail.com>
2024-03-15 17:21:42 +0200

    Updated vet to use JPA. Closes #37

--- pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/log/devdotlog/petclinic/model/Vet.java
@@ -1,9 +1,18 @@
 package log.devdotlog.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    /**
+     * by default M to M & O to M is lazy initialised, when eager JPA will load everything all at once
+     * lazy will not load this until it is asked for, the specialist entity will be null if not eager
+     */
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
