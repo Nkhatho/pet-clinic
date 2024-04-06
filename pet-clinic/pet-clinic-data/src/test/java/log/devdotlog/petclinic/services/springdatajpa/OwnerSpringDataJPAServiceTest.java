9dcc983ed38f42de5f9b9a573a8bb0952a4814af
leole <leolenkhatho@gmail.com>
2024-04-06 08:07:07 +0200

    CRUD tests for Owner Spring Data JPA Service. Closes #50

--- pet-clinic/pet-clinic-data/src/test/java/log/devdotlog/petclinic/services/springdatajpa/OwnerSpringDataJPAServiceTest.java
+++ pet-clinic/pet-clinic-data/src/test/java/log/devdotlog/petclinic/services/springdatajpa/OwnerSpringDataJPAServiceTest.java
@@ -12,16 +12,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerSpringDataJPAServiceTest {
-    public final String LAST_NAME = "Smith";
     @Mock
     OwnerRepository ownerRepository;
     @Mock
@@ -30,6 +32,8 @@ class OwnerSpringDataJPAServiceTest {
     PetTypeRepository petTypeRepository;
     @InjectMocks
     OwnerSpringDataJPAService ownerSpringDataJPAService;
+
+    public final String LAST_NAME = "Smith";
     Owner owner;
 
     @BeforeEach
@@ -37,17 +41,23 @@ class OwnerSpringDataJPAServiceTest {
         owner = Owner.builder().id(1L).lastName(LAST_NAME).build();
         ownerSpringDataJPAService.save(owner);
     }
-
     @Test
     void findByLastName() {
         when(ownerRepository.findByLastName((any()))).thenReturn(owner);
         Owner smith = ownerSpringDataJPAService.findByLastName(LAST_NAME);
         assertEquals(LAST_NAME, smith.getLastName());
     }
-
     @Test
     void findAll() {
+        Set<Owner> owners = new HashSet<>();
+        owners.add(owner);
+        owners.add(Owner.builder().id(2L).build());
 
+        when(ownerRepository.findAll()).thenReturn(owners);
+        Set<Owner> ownerSet = ownerSpringDataJPAService.findAll();
+
+        assertNotNull(ownerSet);
+        assertEquals(2, ownerSet.size());
     }
 
     @Test
@@ -56,16 +66,23 @@ class OwnerSpringDataJPAServiceTest {
         Owner owner = ownerSpringDataJPAService.findById(1L);
         assertEquals(this.owner, owner);
     }
-
     @Test
     void save() {
-    }
+        Owner localOwner = Owner.builder().id(2L).firstName("Tom").build();
+        when(ownerRepository.save(any())).thenReturn(localOwner);
+        Owner returnedOwner = ownerSpringDataJPAService.save(localOwner);
 
+        assertNotNull(returnedOwner);
+        assertEquals(localOwner, returnedOwner);
+    }
     @Test
     void delete() {
+        ownerSpringDataJPAService.delete(owner);
+        verify(ownerRepository).delete(any());
     }
-
     @Test
     void deleteById() {
+        ownerSpringDataJPAService.deleteById(1L);
+        verify(ownerRepository).deleteById(anyLong());
     }
 }
